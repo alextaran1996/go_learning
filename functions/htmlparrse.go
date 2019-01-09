c6508a49d5effd7acd5e3e33160b887b9de4db1d
alextaran1996 <alex_taran@bk.ru>
2019-01-09 23:42:28 +0300

    parse html for links

--- functions/htmlparrse.go
+++ functions/htmlparrse.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"golang.org/x/net/html"
 )
+
+// Get all links founded on the page
+func readlinks(links []string, n *html.Node) []string {
+
+	for _, val := range n.Attr {
+		if val.Key == "href" {
+			links = append(links, val.Val)
+		}
+	}
+	for child := n.FirstChild; child != nil; child = child.NextSibling {
+		links = readlinks(links, child)
+	}
+	return links
+}
 
 const htmlURL = "https://golang.org"
 
@@ -19,15 +32,18 @@ func main() {
 	// resp.Body returns *http.http2gzipReader Reader type
 	// Reader get source data and put it in transfer buffer, after returns pointer to this transfer buffer
 	// ioutil.ReadAll read data from transfer buffer and returns byte slice
-	body, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		fmt.Println("Can not get body from response:", err)
-	}
-	fmt.Printf("%s", body)
+	// body, err := ioutil.ReadAll(resp.Body)
+	// resp.Body.Close()
+	// if err != nil {
+	// 	fmt.Println("Can not get body from response:", err)
+	// }
+	// fmt.Printf("%s", body)
 	parse, err := html.Parse(resp.Body)
 	if err != nil {
 		fmt.Println("Can not parse response:", err)
 	}
-	fmt.Printf("%T", parse)
+	// fmt.Println(parse)
+	for _, val := range readlinks(nil, parse) {
+		fmt.Println(val)
+	}
 }
