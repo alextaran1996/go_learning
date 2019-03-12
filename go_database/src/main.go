53e145c3be43fcc4a57b58be4c5f8c1e18892b48
alextaran1996 <alex_taran@bk.ru>
2019-03-13 00:35:37 +0300

    Added README.md and fixed flag issue

--- go_database/src/main.go
+++ go_database/src/main.go
@@ -5,18 +5,16 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 )
-
-// Agenda
-// Call functions using flags from CLI
-// Idea: create one more layer of functions that will get neccessary arguments and use functions
 
 func main() {
 	config := flag.String("c", "config.yml", "Path to to config.yml file")
-	// newid := flag.String("n", "", "Create new record.Use -n Hleb")
-	// updateid := flag.String("u", "", "Update existing value. Use -u 2 Ann")
+	newid := flag.String("n", "", "Create new record.Use -n Hleb")
+	updateid := flag.String("u", "", "Update existing value. Use -u 2,Ann")
 	deleteid := flag.Int("d", 0, "Delete specified id.Use -d 1")
-	// getusers := flag.String("g", "", "Get all records.Use -g")
+	getuser := flag.String("g", "", "Get all records.Use -g")
 	flag.Parse()
 	db, err := readconfig(*config)
 	if err != nil {
@@ -28,12 +26,24 @@ func main() {
 		log.Fatal(err)
 	}
 	defer conn.Close()
-	// name := User{Name: *newid}
-	// createuser(conn, db, &name)
-	if *deleteid == 0 {
-		log.Fatalln("id value shoud be setted and be grater then 0")
+	if *deleteid != 0 {
+		deleteID(conn, db, *deleteid)
 	}
-	// deleteID(conn, db, *deleteid)
-	// updateID(conn, &db, 2, "Zoom")
-	// getusers(conn, db)
+	if *newid != "" {
+		name := User{Name: *newid}
+		createuser(conn, db, &name)
+	}
+	if *updateid != "" {
+		fmt.Println(*updateid)
+		id := strings.Split(*updateid, ",")
+		idval, err := strconv.Atoi(id[0])
+		if err != nil {
+			log.Fatal("Can not convert string.See examles")
+		}
+		updateID(conn, db, idval, id[1])
+	}
+	if *getuser == "yes" {
+		getusers(conn, db)
+	}
+
 }
