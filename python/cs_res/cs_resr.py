96dabdd669ea9b2deb083403549f9f3a1fdd2c2a
alextaran1996 <alex_taran@bk.ru>
2019-06-04 11:27:46 +0300

    Final version

--- python/cs_res/cs_resr.py
+++ python/cs_res/cs_resr.py
@@ -1,40 +1,53 @@
-#!/usr/bin/env python
-from cs import CloudStack, read_config
+from cs import CloudStack
 import json
 import sys
+from ruamel.yaml import YAML
 
 class load_resources(object):
-    def __init__(self, tenant,cs):
+    def __init__(self, tenant,cs,logfiledescr):
         self.tenant = tenant
-        self.limits_used = {}
-        self.limits_total = {}
+	self.logfiledescr = logfiledescr
+        self.limits_all = {}
         self.cs = cs
 
     def get_resources(self):
         self.projectid=self.cs.listProjects(name=self.tenant)['project'][0]['id']
         limits = self.cs.listProjects(name=self.tenant)['project'][0]
-        self.limits_used = {"memory": limits['memorytotal'],"cpu": limits['cputotal'],"primarystorage": limits['primarystoragetotal'],"volume": limits["volumetotal"],"secondarystorage": limits['secondarystoragetotal']}
-        self.limits_total = {"volume": limits['volumelimit'],"primarystorage": limits['primarystoragelimit'],"cpu": limits['cpulimit'],"memory": limits['memorylimit'],"secondarystorage": limits['secondarystoragelimit']}
+        for limit in limits:
+         if limits[limit] == 'Unlimited':
+          limits[limit] = 1000000000
+        self.limits_all = {"memory_total": limits['memorytotal'],"cpu_total": limits['cputotal'],"primarystorage_total": limits['primarystoragetotal'],"volume_total": limits["volumetotal"],"secondarystorage_total": limits['secondarystoragetotal'],"volume_used": limits['volumelimit'],"primarystorage_used": limits['primarystoragelimit'],"cpu_used": limits['cpulimit'],"memory_used": limits['memorylimit'],"secondarystorage_used": limits['secondarystoragelimit']}
 
         
     def write_to_prom(self):
-        with open('res.prom',"a") as prom_log:
-            for res in self.limits_total:
-                logstring_used = "cloudstack_%s_used{project=%s} %s\n"%(res,self.tenant,self.limits_used.get(res))
-                logstring_total = "cloudstack_%s_total{project=%s} %s\n"%(res,self.tenant,self.limits_total.get(res))
-                prom_log.write(logstring_used)
-                prom_log.write(logstring_total)
+        for res in self.limits_all:
+         limitsall[res] += "cloudstack_%s{project=\"%s\"} %s\n"%(res,self.tenant,self.limits_all.get(res))
+	 
 
 
-
-endpoint = 'http://endpoint'
-apikey = 'your apikey'
-secretkey = 'your secretkey'
-cs = CloudStack(endpoint=endpoint,key=apikey,secret=secretkey)
+with open(sys.argv[1],'r') as config:  # Read data from config file
+    yaml = YAML()
+    data = yaml.load(config)
+    limitsall = {
+"memory_total": "# HELP  cloudstack_memory_total Total amount of allocated memory that can be used by this project\n# TYPE  cloudstack_memory_total gauge\n",
+"cpu_total": "# HELP  cloudstack_cpu_total Total number of allocated vCPU  can be used by this project \n# TYPE  cloudstack_cpu_total gauge\n",
+"primarystorage_total": "# HELP  cloudstack_primarystorage_total Total space allocated on primary storage\n# TYPE cloudstack_primarystorage_total gauge\n",
+"volume_total": "# HELP  cloudstack_volume_total Total number of disk volumes that can be created by this project\n# TYPE  cloudstack_volume_total gauge\n",
+"secondarystorage_total": "# HELP  cloudstack_secondarystorage_total Total space allocated on secondary storage\n# TYPE  cloudstack_secondarystorage_total gauge\n",
+"memory_used": "# HELP  cloudstack_memory_used Amount of memory used by the project\n# TYPE cloudstack_memory_used gauge\n",
+"cpu_used": "# HELP  cloudstack_cpu_used Number of cores used by the project\n# TYPE  cloudstack_cpu_used gauge\n",
+"primarystorage_used": "# HELP  cloudstack_primarystorage_used Amount of primarystorage used by th eproject \n# TYPE  cloudstack_primarystorage_used gauge\n",
+"volume_used": "# HELP  cloudstack_volume_used Number of disk volumes used by this ptoject\n# TYPE  cloudstack_volume_used gauge\n",
+"secondarystorage_used": "# HELP  cloudstack_secondarystorage_used Amount of secondarystorage used by the project\n# TYPE  cloudstack_secondarystorage_used gauge\n"}
+logfiledescr = open(data['logfile'],'w+') # File is opened here because it should be overwritten every time when script executes
+cs = CloudStack(endpoint=data['endpoint'],key=data['api'],secret=data['secret'])
 projects_list = list()
-for project in cs.listProjects()['project']:
+for project in cs.listProjects()['project']: # gather list of tenants
     projects_list.append(project['displaytext'])
 for project in projects_list:
-    RESOURCES = load_resources(project,cs)
+    RESOURCES = load_resources(project,cs,logfiledescr)
     RESOURCES.get_resources()
-    RESOURCES.write_to_prom()
+    RESOURCES.write_to_prom()	
+for values in limitsall: 
+    logfiledescr.write(limitsall[values])
+logfiledescr.close()
--- python/pim_xen/pim_xen.py
+++ python/pim_xen/pim_xen.py
@@ -1,90 +1,87 @@
-import requests
-import json
 import XenAPI
-import socket 
 import re
-# Steps:
-#   1.Get list of master nodes 
-#   2.For every master node:
-#       3. Create session for remote XenAPI
-#       4. Get list ov vms from PIM
-#       5. Get list of vms from XenAPI
+import requests
+import sys
+from ruamel.yaml import YAML
 
-class GetPimVms(object): # Create a new class;  
+class GetPimVms(object): 
+    def __init__(self,hostname,logfiledescr):
+	    self.hostname = hostname
+        self.logfiledescr = logfiledescr  
+    # Create session to connect to remote XenAPI
+	def create_session(self,url, username, password):
+        session = XenAPI.Session(url)
+        session.xenapi.login_with_password(username, password)
+        return session
 
-    def get_list_master_pool(filepath):
-        with open(filepath) as fp:
-            master_nodes = fp.read().splitliens()
-        return master_nodes
-
-
-    # Step 2.Connect to remote xenapi    
-    def create_session(url, username, password):
-        try:
-            session = XenAPI.Session(url, ignore_ssl=True)
-            print session
-            session.xenapi.login_with_password(username, password)
-        except Exception, e:
-            if hasattr(e, 'details') and e.details[0] == 'HOST_IS_SLAVE':
-                # Redirect to cluster master
-                url = urlparse(url).scheme + '://' + e.details[1]
-                print url
-
-                try:
-                    session = XenAPI.Session(url, ignore_ssl=True)
-                    session.login_with_password(username, password)
-                except Exception, e:
-                    handle_login_error(e)
-            else:
-                handle_login_error(e)
-        return (session)
-
-    def _list_of_vms_pim(self): # Step 3.List of vms from PIM
-        vms = list()
+	#List of vms from PIM
+    def _list_of_vms_pim(self):
+            vms = list()
         return_list = list()
-        sub_part_host = self.poolname.split(".")[0].split("-")[1] # Get part of the hostname ex: xen1
+        sub_part_host = self.hostname.split(".")[0].split("-")[1] # Get part of the hostname ex: xen1
         sub_part_host_res = re.sub(r"\d+","*",sub_part_host) # Substitute all numbers in sub_part_host to * ex: xen*
-        regexp = re.sub(sub_part_host,sub_part_host_res,self.poolname) # Make regexp for pim
-        query = "http://your_cloudstack_server/v1/asset?keyword={}&assetTypeId=2&deleted=false&highlight=false&aggregation=false&resultsPerPage=10000".format(regexp) # Change value to poolname
+        regexp = re.sub(sub_part_host,sub_part_host_res,self.hostname) # Make regexp for pim
+        query = "http://(HOSTNAME)/(X)/v1/asset?keyword={}&assetTypeId=2&deleted=false&highlight=false&aggregation=false&resultsPerPage=10000".format(regexp) # Change value to poolname
         rdata = requests.get(query)
         rdata = rdata.json()
         for items in rdata['items']:
-            for child_links in items['links']['children']:
-                vms.append(child_links) # need to create a list,as child_links is a string of links to child objects and I can't work with them directly 
-            for vmname in vms:
-                if vmname['assetTypeId'] == 1 and vmname['displayName'] not in return_list:
-                    return_list.append(vmname['displayName'])
-        for name in return_list:
-            print(name)
-    
-    
-    def _list_of_vms_hv(self,session): # Step 4.List of vms from hypervisor
+        for child_links in items['links']['children']:
+            vms.append(child_links) # need to create a list,as child_links is a string of links to child objects and I can't work with them directly 
+        for vmname in vms:
+            if vmname['assetTypeId'] == 1 and vmname['displayName'] not in return_list:
+            return_list.append(str(vmname['displayName'])) # need to convert to str as default type of vmname['displayName'] is unicode
+        return return_list
+        
+    def _list_of_vms_hv(self,session): #List of vms from hypervisor
         vmArray = []
         vms = session.xenapi.VM.get_all() #Get all the vms running on the xen hypervisor
         # Loop through each VM in vms, we'll skip it if the vm is a template, control domain, or a snapshot
         for vm in vms:
-                if session.xenapi.VM.get_is_a_template(vm):
-                        continue
-                elif session.xenapi.VM.get_is_control_domain(vm):
-                        continue
-                elif session.xenapi.VM.get_is_a_snapshot(vm):
-                        continue
-                else:
-                        if session.xenapi.VM.get_name_label(vm) not in vmArray:
-                                vmArray.append(session.xenapi.VM.get_name_label(vm))
+        if session.xenapi.VM.get_is_a_template(vm):
+            continue
+        elif session.xenapi.VM.get_is_control_domain(vm):
+            continue
+        elif session.xenapi.VM.get_is_a_snapshot(vm):
+            continue
+        else:
+            if session.xenapi.VM.get_name_label(vm) not in vmArray:
+            vmArray.append(session.xenapi.VM.get_name_label(vm))
+        return vmArray
+        #!!!!!!!!!!!!!!!!! Need to close the session !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
 
+    def comp(self,list1, list2): #Compare lists 
+	    diff = list(set(list1).symmetric_difference(set(list2))) # Values that are not in both sets
+	    if len(diff) != 0:
+          self.notif(diff)
+        
+        # Write difference in result file 
+    def notif(self,diff):
+        for i in diff:
+	        output['diff'] += "xapi_pim_diff{master=\"" + self.hostname + "\",vm=\""+ i + "\"} 1\n"  # 1 means that
 
-    def comp(self,list1, list2): # Step 5.Compare lists 
-        difference = list()
-        for val in list1: 
-            if val in list2: 
-                continue
-            else: 
-                difference.append(val)
-        print(difference)
-        # check alternative difference.append(set(list1) & set(list2))
-                  
-
-
-OBJECTS = GetPimVms(create_session(""))
-OBJECTS.comp(OBJECTS._list_of_vms_pim(),OBJECTS._list_of_vms_hv())
+with open(sys.argv[1],'r') as config:  # Read data from config file
+ yaml = YAML()
+ data = yaml.load(config)
+output = {
+  "diff": "# HELP xapi_pim_diff Difference in the info provided by XenAPI and PIM.\n# TYPE xapi_pim_diff  gauge\n",
+  "auth_fail": "# HELP xen_authentication_fail Failed to authenticate to XenAPI.\n# TYPE xen_authentication_fail gauge\n"
+}
+logfiledescr = open(data['logfile'],'w+') # File is opened here because it should be overwritten every time when script executes
+masters = list()
+query = "http://(HOSTNAME)/(X)/v1/asset?keyword=(TEMPLATEE)&assetTypeId=2&deleted=false&highlight=false&aggregation=false&resultsPerPage=10000" 
+rdata = requests.get(query)
+rdata = rdata.json()
+for i in rdata['items']: # Here we gather all pool that fit the pattern pool*-xen1.sys.ams1.cloudsys.tmcs. They will be used for comparing
+ masters.append(i['doc']['Name'])
+for master in masters: 
+ url = "http://" + str(master) # Create url string to connect to xenapi
+ OBJECT = GetPimVms(master,logfiledescr) # as a second param file descriptor is passed to write result file
+ try:
+  session = OBJECT.create_session(url,data['user'],data['password'])
+ except:
+  output['auth_fail'] += 'xen_pim_authentication_fail{pool="%s"} 1\n'%(master)
+  continue
+ OBJECT.comp(OBJECT._list_of_vms_pim(),OBJECT._list_of_vms_hv(session)) # compare PIM list and Xen list
+for value in output:
+ logfiledescr.write(output[value])
+logfiledescr.close() # Close file
